23a9ae210c08140081228b7f86f6d198b9dc10f7
kyoung <kyoungkilsby@gmail.com>
2022-10-09 08:06:13 -0400

    add employee and state List

--- src/main/java/com/cydeo/EmployeeController.java
+++ src/main/java/com/cydeo/EmployeeController.java
@@ -1,14 +1,21 @@
 package com.cydeo;
 
+import com.cydeo.bootstrap.DataGenerator;
+import com.cydeo.model.Employee;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/employee")
 public class EmployeeController {
+
+
     @GetMapping("/register")
-    public String creatEmployee(){
+    public String creatEmployee(Model model){
+        model.addAttribute("employee", new Employee());
+        model.addAttribute("stateList", DataGenerator.getAllStates());
         return "employee/employee-create";
     }
 }
